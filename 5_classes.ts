637ebd03fd036f724e364d77e486f72319ce09ba
tiger3120 <zhek4770@gmail.com>
2021-09-26 23:28:06 +0300

    Started classes file

---
+++ 5_classes.ts
@@ -0,0 +1,11 @@
+class Typescript {
+    version: string
+
+    constructor(version: string) {
+        this.version = version
+    }
+
+    info(name: string) {
+        return `[${name}]: Typescript version is ${this.version}`
+    }
+}
