ea6f130ad7b02b3f4ca33793d1d93bfede047fad
tiger3120 <zhek4770@gmail.com>
2021-09-26 23:47:08 +0300

    Finished this file

--- 5_classes.ts
+++ 5_classes.ts
@@ -28,3 +28,43 @@ class Car {
 }
 
 // =========================
+
+class Animal {
+    protected voice: string = ''
+    public color: string = 'black'
+
+    constructor() {
+        this.go()
+    }
+
+    private go() {
+        console.log('Go');
+    }
+}
+
+class Cat extends Animal {
+    public setVoice(voice: string) {
+        this.voice = voice
+    }
+}
+
+const cat = new Cat()
+cat.setVoice('test')
+console.log(cat.color)
+
+// ======================
+
+abstract class Component {
+    abstract render(): void
+    abstract info(): string
+}
+
+class AppComponent extends Component {
+    render(): void {
+        console.log('Component on render')
+    }
+
+    info(): string {
+        return 'This is info'
+    }
+}
